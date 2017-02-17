d233ed9e39cf9628ed4f816b74877448eaa438c5
Ross Schlaikjer <ross@schlaikjer.net>
2017-02-16 19:17:53 -0500

    Spec out records for trace data

---
+++ include/records.hrl
@@ -0,0 +1,36 @@
+-record(version_header, {
+          data_file_overflow :: 'true' | 'false',
+          clock_type :: 'dual',
+          elapsed_time :: pos_integer(),
+          num_method_calls :: pos_integer(),
+          clock_call_overhead :: pos_integer(),
+          vm :: 'art' | 'dalvik',
+          pid :: pos_integer()
+         }).
+
+-record(trace_thread, {
+          thread_id :: pos_integer(),
+          thread_name :: binary()
+         }).
+
+-record(trace_method, {
+          method_id :: pos_integer(),
+          class_name :: binary(),
+          method_name :: binary(),
+          signature :: binary(),
+          source_file :: binary()
+         }).
+
+-record(records_header, {
+          version :: 1..3,
+          data_offset :: pos_integer(),
+          start_offset :: pos_integer(),
+          record_size :: pos_integer()
+         }).
+
+-record(call_record, {
+          thread_id :: pos_integer(),
+          method_id :: pos_integer(),
+          time_delta :: pos_integer(),
+          wall_time_delta :: pos_integer()
+         }).
